f63f518220b06caf4fa13d97b333cd5dfd23757d
bacodelab <56372205+bacodelab@users.noreply.github.com>
2020-08-10 14:19:51 +0300

    Add files via upload

---
+++ Paket-1 Giriş Seviye Uygulamalar/İf-Else Komutu Kullanımı.py
@@ -0,0 +1,18 @@
+"""
+İf Komutu Kullanımı - Else ile birlikte
+"""
+
+print("x Kafe Yaş Kontrol Sistemi")
+
+yas = int(input("Yaşınızı Giriniz:"))
+
+if (yas < 18):
+    print("Yaşınız:",yas)
+    print("Bu Mekana Giremezsiniz.")
+else:
+    print("Yaşınız:",yas)
+    print("Mekana Hoşgeldiniz.")
+
+
+
+
